598e537db9639a475a4e01e5233995d670b465e4
Paris Kasidiaris <paris@sourcelair.com>
2017-11-01 18:19:03 +0200

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import { Terminal } from 'xterm/src/Terminal';
+
+
+let term = new Terminal();
+
+// // Expose to window for debugging
+// (<any>window).term = term;
+
+// term.open(document.getElementById('xterm-container'));
